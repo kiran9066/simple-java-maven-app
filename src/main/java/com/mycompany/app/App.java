4a2942142c8ccd199bd631f13fb6ce026b655f2c
kiran9066 <136839659+kiran9066@users.noreply.github.com>
2023-07-10 21:34:39 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello its new World!";
+    private static final String MESSAGE = "HelloWorld!";
 
     public App() {}
 
